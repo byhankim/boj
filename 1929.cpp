cfd3a8446fcf01ad118982dfb8835b2d463e7db6
hankim <byhankim>
2020-04-27 18:13:11 +0900

    [1929] 소수 구하기

--- 1929.cpp
+++ 1929.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(void)
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> v;
     bool is_prime;
     int M, N;
     cin >> M >> N;
-    for (int i = M; i <= N; i++)
+    for (int i = 2; i < M; i++)
     {
         is_prime = true;
-        if (i == 1) i++;
         for (int j = 2; j * j <= i; j++)
         {
             if (i != j && i % j == 0)
@@ -17,7 +20,25 @@ int main(void)
                 break;
             }
         }
-        if (is_prime) cout << i << '\n';
+        if (is_prime) v.push_back(i);
+    }
+    for (int i = M; i <= N; i++)
+    {
+        is_prime = true;
+        if (i == 1) i++;
+        for (vector<int>::iterator it = v.begin(); it != v.end() && ((*it) * (*it) <= i); it++)
+        {
+            if (*it != i && i % *it == 0)
+            {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) 
+        {
+            v.push_back(i);
+            cout << i << '\n';
+        }
     }
     return 0;
 }
