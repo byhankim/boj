065373dfe822542e6712b07fca007e43d7188ba7
hankim <byhankim>
2020-04-28 22:54:09 +0900

    [8393] 합

---
+++ 8393.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(void)
+{
+    int N;
+    std::cin >> N;
+    if (N % 2 == 0)
+        std::cout << (N + 1) * int(N / 2);
+    else
+        std::cout << (N + 1) * int(N / 2) + (1 + N) / 2;
+    // std::cout << (N % 2 == 0) ? (N + 1) * (N / 2) : (N + 1) * N / 2 + (1 + N) / 2;
+    return 0;
+}
