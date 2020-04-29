5087a6abfe2b5e3cfa50784ca995aaa7ea9a16c6
hankim <byhankim>
2020-04-30 00:23:18 +0900

    [4948] 베르트랑 공준

---
+++ 4948.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N, cnt, idx = 123457 * 2;
+    bool is_prime, parr[idx];
+    for (int i = 2; i < idx; i++) parr[i] = true;
+    parr[0] = parr[1] = false;
+    while (cin >> N && N != 0)
+    {
+        cnt = 0;
+        for (int i = 2, idx = N * 2; i <= idx; i++)
+        {
+            if (!parr[i]) continue;
+            for (int j = 2 * i; j <= idx; j+=i) parr[j] = false;
+            if (i > N && parr[i]) cnt++;
+        }
+        cout << cnt << '\n';
+    }
+}
