390e66696f4a89ef92751d099594358805b39764
hankim <byhankim>
2020-04-30 02:33:13 +0900

    [4948] 베르트랑 공준

--- 4948.cpp
+++ 4948.cpp
@@ -5,7 +5,7 @@ int main(void)
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N, cnt, idx = 123457 * 2;
-    bool is_prime, parr[idx];
+    bool parr[idx];
     for (int i = 2; i < idx; i++) parr[i] = true;
     parr[0] = parr[1] = false;
     while (cin >> N && N != 0)
