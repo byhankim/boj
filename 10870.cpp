b6b15c2820b3cd0165a2cd49416aa6a44911ce56
hankim <byhankim>
2020-05-02 02:38:58 +0900

    [10870] 피보나치 수 5

---
+++ 10870.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n < 1) return 0;
+    else if (n < 3) return 1;
+    return (fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+}
