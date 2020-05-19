121435d7b534b36615d8b9e64598e82c5aed3b23
hankim <byhankim>
2020-05-19 17:00:25 +0900

    [11022] A+B-8

---
+++ 11022.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A << " + " << B << " = " << A+B << '\n'; 
+    }
+    return 0;
+}
